97086b168226e4755f5c24955cf876679cdd6b18
Rebecca Overton <rebeccaoverton@Rebeccas-Air.attlocal.net>
2022-10-30 23:13:35 -0700

    requirements added to js file, prompts.js created

---
+++ promps.js
@@ -0,0 +1,12 @@
+//File to store the inquirer promps
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+    { 
+        type: 'list',
+        name:,
+        message:,
+        choices: []
+    },
+]);
--- server.js
+++ server.js
@@ -1,29 +1,21 @@
-const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const prompts = require('./promps');
+const table = require('console.table');
 
-const PORT = process.env.PORT || 3000;
-const app = express();
-
-//Express middleware
-app.use(express.urlencoded({extended : false}));
-app.use(express.json());
 
 //Connect to database
-const db = mysql.createConnection(
-    {
-        // host : 'local server',
-        host : '127.0.0.1',
-        user : 'root',
-        password : 'password',
-        database : 'employee_db'
-    },
-    console.log(`Successfully connected to employee_db`)
-);
+// const db = mysql.createConnection(
+//     {
+//         // host : 'localhost',
+//         host : '127.0.0.1',
+//         port : 3000,
+//         user : 'root',
+//         password : 'password',
+//         database : 'employee_db'
+//     },
+//     console.log(`Successfully connected to employee_db`)
+// );
 
 //Query functions for database
 
-
-//Listener
-app.listen(PORT, () =>{
-    console.log(`Listening at http://localhost:${PORT}`)
-});
