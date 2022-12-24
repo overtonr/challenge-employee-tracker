61a9256855a8cf78118ef58754ee61ee95d8f9f5
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-12-23 17:07:17 -0800

    schema finalized

--- db/schema.sql
+++ db/schema.sql
@@ -3,20 +3,23 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
+DROP TABLE IF EXISTS department;
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  dep_name VARCHAR(30),
+  dep_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS role;
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
+  salary DECIMAL NOT NULL,
   dep_id INT NOT NULL,
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
